24bec78c11cce15b37b613f58cbe526fab2c0b82
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-04 14:44:37 +0700

    ### Fix failed pipeline

--- src/sorting/insertion_sort.js
+++ src/sorting/insertion_sort.js
@@ -1,10 +1,17 @@
+/**
+ * insertionSort
+ * @param {array} arr - value
+ * @return {array} array sorting
+ */
 function insertionSort(arr) {
-  var currentVal;
-  for (var i = 1; i < arr.length; i++) {
+  let currentVal;
+  for (let i = 1; i < arr.length; i++) {
     currentVal = arr[i];
+    // eslint-disable-next-line no-var, vars-on-top
     for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
+    // eslint-disable-next-line block-scoped-var
     arr[j + 1] = currentVal;
   }
   return arr;
